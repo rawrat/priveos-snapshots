732495286a84c02b013c68dfc0981982307e3317
Angelo Laub <angelolaub@gmail.com>
2019-04-16 21:32:44 +0200

    use ipfs.io instead if cloudflare (doesn’t seem to work)

--- src/frontend.js
+++ src/frontend.js
@@ -6,13 +6,13 @@ const server = restify.createServer()
 server.get('/', async function(req, res, next) {
   const hash = (await fs.readFile('latest.txt', 'utf8')).trim()
   
-  res.send(`Ohai Latest hash is ${hash}`)
+  res.send(`Latest hash is ${hash}`)
   next()
 })
 
 server.get('/latest', async function(req, res, next) {
   const hash = (await fs.readFile('latest.txt', 'utf8')).trim()
-  res.redirect(301, `https://cloudflare-ipfs.com/ipfs/${hash}`, next)
+  res.redirect(301, `https://ipfs.io/ipfs/${hash}`, next)
 })
 
 server.listen(3333, "127.0.0.1", function() {

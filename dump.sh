4b25f23023b900f93ebfbb783880f90db9489dd9
Angelo Laub <angelolaub@gmail.com>
2019-04-16 20:25:37 +0200

    publish tarball to ipfs & add frontend

--- dump.sh
+++ dump.sh
@@ -1,7 +1,19 @@
-#!/bin/sh
+#!/bin/sh -e
 
+# this will create a dump dir in the local path
 mongodump --excludeCollection=data --excludeCollection=state_history -d priveos
 
+# this will create a file named priveos-ipfs-snapshot.sqlite
 node src/dump_ipfs.js
 
-tar cfz "priveos_snapshot_$(date +%Y-%m-%d_%H:%M).tar.gz" dump priveos-ipfs-snapshot.sqlite 
+tarball="priveos_snapshot_$(date +%Y-%m-%d_%H:%M).tar.gz"
+
+tar cfz $tarball dump priveos-ipfs-snapshot.sqlite 
+
+# remove temporary files from above
+rm priveos-ipfs-snapshot.sqlite 
+rm -rf dump
+
+# publish the tarball
+hash=$(ipfs --api=/ip4/127.0.0.1/tcp/5001 add -Q $tarball)
+echo $hash > latest.txt
---
+++ src/frontend.js
@@ -0,0 +1,22 @@
+'use strict'
+const restify = require('restify')
+const fs = require('fs').promises
+const server = restify.createServer()
+server.use(restify.plugins.bodyParser())
+
+server.get('/', async function(req, res, next) {
+  const hash = (await fs.readFile('latest.txt', 'utf8')).trim()
+  
+  res.send(`Ohai Latest hash is ${hash}`)
+  next()
+})
+
+server.get('/latest', async function(req, res, next) {
+  const hash = (await fs.readFile('latest.txt', 'utf8')).trim()
+  res.redirect(301, `https://cloudflare-ipfs.com/ipfs/${hash}`, next)
+})
+
+server.listen(3333, "127.0.0.1", function() {
+  console.log('Listening on port 3333')
+  
+})

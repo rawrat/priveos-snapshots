676a4a72bba9f78aad9ae1e7493efbde25355130
Angelo Laub <angelolaub@gmail.com>
2019-04-16 19:19:56 +0200

    improve restore script

--- src/restore_ipfs.js
+++ src/restore_ipfs.js
@@ -6,8 +6,11 @@ const dbName = 'priveos2'
 const mongo = new Mongo(mongoUrl, dbName)
 const ipfsClient = require('ipfs-http-client')
 const assert = require('assert')
+const _ = require('underscore')
 const Bluebird = require('bluebird')
 Bluebird.promisifyAll(sqlite3)
+
+const CHUNK_SIZE = 500
 
 const config = {
   ipfsConfig: {
@@ -22,13 +25,28 @@ async function main() {
   const sqlite = new sqlite3.Database('priveos-ipfs-snapshot.sqlite')
   const db = await mongo.db()
   
-  await sqlite.eachAsync("select * from ipfs", async (err, row) => {
-    const results = await ipfs.add(Buffer.from(row.data), {pin: true})
-    const hash = results[0].hash
-    assert.equal(hash, row.hash)
-  })
+  let offset = 0
+  let count = 0
+  while(true) {
+    const rows = await sqlite.allAsync("select * from ipfs order by hash limit ? offset ?",[CHUNK_SIZE, offset])  
+    
+    if(!rows.length) {
+      break
+    }
+    
+    const files = rows.map(row => Buffer.from(row.data))
+    const results = await ipfs.add(files, {pin: true})
+    for (const [row, result] of _.zip(rows, results)) {
+      // console.log(`row.hash: ${row.hash} result.hash: ${result.hash}`)
+      assert.equal(row.hash, result.hash)
+      count++
+    }
+    
+    offset += CHUNK_SIZE
+    // console.log("Finished! offset: ", offset)
+  }
   
-  console.log("Finished!")
+  console.log("Total number of hashes: ", count)
 
   sqlite.close()
   mongo._connection.close()

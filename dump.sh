b06de26cacaaab90996016032e58f4aa493bb75a
Angelo Laub <angelolaub@gmail.com>
2019-04-16 18:20:24 +0200

    make tarball out of it

--- dump.sh
+++ dump.sh
@@ -3,3 +3,5 @@
 mongodump --excludeCollection=data --excludeCollection=state_history -d priveos
 
 node src/dump_ipfs.js
+
+tar cfz "priveos_snapshot_$(date +%Y-%m-%d_%H:%M).tar.gz" dump priveos-ipfs-snapshot.sqlite 

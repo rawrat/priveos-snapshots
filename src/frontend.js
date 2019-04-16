91b74a451b63a544b526e02f61fcdef5be39349f
Angelo Laub <angelolaub@gmail.com>
2019-04-16 22:02:28 +0200

    beautify

--- src/frontend.js
+++ src/frontend.js
@@ -5,8 +5,9 @@ const server = restify.createServer()
 
 server.get('/', async function(req, res, next) {
   const hash = (await fs.readFile('latest.txt', 'utf8')).trim()
-  
-  res.send(`Latest hash is ${hash}`)
+  const filename = hash.split('/')[1]
+  res.header('Content-Type', 'text/html')
+  res.sendRaw(`<!DOCTYPE html><html>Latest privEOS snapshot: <a href="https://ipfs.io/ipfs/${hash}">${filename}</a><br>You can always get the latest snapshot by following this link: <a href="https://snapshots.priveos.io/latest">https://snapshots.priveos.io/latest</a>`)
   next()
 })
 
